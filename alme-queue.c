6ef40a7d248284eb021cbcbf57b70db8412fb242
neuromanqe <agonhoxha99@gmail.com>
2021-10-09 09:53:58 -0400

    10/09/2021

--- alme-queue.c
+++ alme-queue.c
@@ -12,6 +12,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "queue.h"
 
 
@@ -98,13 +99,49 @@ void* qget(queue_t *qp) {
 	}
 	
 }
+void* qsearch(queue_t *qp, bool (*searchfn)(void* elementp, const void* keyp), const void* skeyp) {
 
+	rq_t *rq;
+	rqe_t *e;
+
+	rq = (rq_t*)qp;
+	e = rq->front;
+	
+	while(e != NULL) {
+		if(searchfn(e->data, skeyp) == true) {
+			return e;
+		}
+		e = e->next;
+	}
+	
+	return NULL;
+
+}
+
+bool srch(void* elementp, const void* keyp) {
+
+	int* a = ((int*)(elementp));
+	int* b = ((int*)(keyp));
+
+	if(*a == *b) {
+		return true;
+	} else {
+		return false;
+	}
+	
+}
 void qapply(queue_t *qp, void(*fn)(void* elementp)) {
 
 	rqe_t* e;
 	rq_t *rq;
 	
-	rq = (rq_t*)qp;  
+	rq = (rq_t*)qp;
+
+	if(rq == NULL) {
+		printf("No queue\n");
+		return;
+	}
+	
 	e = rq->front;
 
 	if(e == NULL){
@@ -118,6 +155,7 @@ void qapply(queue_t *qp, void(*fn)(void* elementp)) {
 	}	
 	
 }
+
 
 static void printe(void* element_p) {
 
@@ -145,17 +183,30 @@ static void print_queue(queue_t* qp) {
 
 int main(void) {
 	queue_t* my_queue = qopen();
-	int num_pt, second_num;
+	rqe_t* search_return;
+	int num_pt, second_num, third_num;
 
 	num_pt = 3;
-	second_num = 10;
+	second_num = 7;
+	third_num = 3;
+
+  bool y = srch((void*)(&second_num), (void*)(&third_num));
+	printf("%d\n", y);
 	
 	qput(my_queue,&num_pt);
-	qput(my_queue, &second_num);
-	qapply(my_queue, printe);
+	//qput(my_queue, &second_num);
+	//qapply(my_queue, printe);
+
+	/*search_return = (rqe_t*)(qsearch(my_queue, srch, &third_num));
+	if(search_return == NULL) {
+		printf("element not found!\n");
+	} else {
+		printf("%d\n", *((int*)search_return->data));
+		}*/
 	
-	qget(my_queue);
-	print_queue(my_queue);
+	//qget(my_queue);
+	//qclose(my_queue);
+	free(my_queue);
 	
 	exit(EXIT_SUCCESS);
 }
