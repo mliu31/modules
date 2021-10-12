13d23eadb0352c64b6980ec4655cc9e4cf6ef0f7
Megan Liu <megan.y.liu.24@dartmouth.edu>
2021-10-12 09:48:09 -0400

    progress on hput

--- hashfile.c
+++ hashfile.c
@@ -17,8 +17,10 @@
 #include "queue.h"
 #include "queue.c"
 #include "hash.h"
+#include "hash.c" 
 
 #define STARTING_ARR_CAPACITY 50
+
 
 typedef struct real_helem_t {
 	queue_t *queue;
@@ -27,16 +29,17 @@ typedef struct real_helem_t {
 
 
 typedef struct real_hashtable_t {
-	rhe_t* elems_array[STARTING_ARR_CAPACITY];
-	// when putting new elem in ht, need to check if ht_length == num_entries; double ht_length and copy elem into new one -- amortized O(1) runtime
+	rhe_t* elems_array[STARTING_ARR_CAPACITY]; 
 	int max_len;
 	int curr_len; 
 	
 } rht_t; 
 
+
 static void printe(void* elementp) {
 	printf("%d-->", *((int*)elementp));
 }
+
 
 hashtable_t *hopen(uint32_t hsize) {
 	rht_t *hp;
@@ -51,46 +54,59 @@ hashtable_t *hopen(uint32_t hsize) {
 	hp->curr_len = 0;
 			
 	for(int i = 0; i < hsize; i++) {
-		//printf("For loop\n");
-
 		if (!(ep = (rhe_t*)calloc(hsize, sizeof(rhe_t)))) {
 			printf("[Error: calloc failed for hash element]\n");
 			return NULL;
 		}
-
 		hp->elems_array[i] = ep;
 		
 		ep->queue = qopen();
 		ep->key = NULL;
 	}
-
-	return (hashtable_t*)hp; 
-	
+	return (hashtable_t*)hp; 	
 }
+
 
 void hclose(hashtable_t *htp) {
 	rht_t *hp;
 	rhe_t *ep;
 
-	hp = (rht_t*)(htp);
+	hp = (rht_t*)htp;
 
 	for(int i = 0; i < hp->max_len; i++) {
 		ep = hp->elems_array[i];
 		qclose(ep->queue);
 		free(ep);
  	}
-
 	free(hp);
-	
 }
+
 
 int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen) {
-	//TODO
+	// when putting new elem in ht, need to check if ht_length == num_entries; double ht_length and copy elem into new one -- amortized O(1) runtime
+	//  key and keylen (arbitrary str--we can choose) into our SuperFastHash function
+	
+	// cast htp as rht_t
+	rht_hp *hp;
+	int hashvalue; 
+
+	hp = (rht_hp*)htp;
+	
+	// call super fast hash and return the value in an int var
+	//   param in superfast hash = param in hput: 
+	//   data = key
+	//   len = keylen
+	//   tablesize = rht->max_len
+
+	
+	// index to that returned value in the hash table and store put the ep into the queue there 
 }
+
 
 void happly(hashtable_t *htp, void (*fn)(void* ep)) {
 	//TODO
 }
+
 
 void *hsearch(hashtable_t *htp,
 							bool (*searchfn)(void* elementp, const void* searchkeyp),
@@ -99,12 +115,14 @@ void *hsearch(hashtable_t *htp,
 	//TODO
 }
 
+
 void *hremove(hashtable_t *htp,
 							bool (*searchfn)(void* elementp, const void* searchkeyp),
 							const char *key,
 							int32_t keylen) {
 	//TODO
 }
+
 
 int main(void) {
 
