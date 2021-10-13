b692fdbb7e2babd6960eb7f1594457098f453cd6
neuromanqe <agonhoxha99@gmail.com>
2021-10-12 20:38:47 -0400

    "did a lot of stuff"

--- hashfile.c
+++ hashfile.c
@@ -15,69 +15,46 @@
 #include <stdbool.h>
 #include <string.h>
 #include "queue.h"
-#include "queue.c"
 #include "hash.h"
-#include "hash.c" 
-
-#define STARTING_ARR_CAPACITY 50
-
-
-typedef struct real_helem_t {
-	queue_t *queue;
-	char *key;
-} rhe_t;
-
+#include "hash.c"
 
 typedef struct real_hashtable_t {
-	rhe_t* elems_array[STARTING_ARR_CAPACITY]; 
-	int max_len;
-	int curr_len; 
-	
+	queue_t** table; 
+	uint32_t len;
 } rht_t; 
-
-
-static void printe(void* elementp) {
-	printf("%d-->", *((int*)elementp));
-}
-
 
 hashtable_t *hopen(uint32_t hsize) {
 	rht_t *hp;
-	rhe_t *ep;
 	
 	if (!(hp = (rht_t*)calloc(1, sizeof(rht_t)))) {
-		printf("[Error: calloc failed for hashtable]\n");
+		printf("[Error: malloc failed for hashtable]\n");
 		return NULL;
 	}
 
-	hp->max_len = hsize;
-	hp->curr_len = 0;
-			
-	for(int i = 0; i < hsize; i++) {
-		if (!(ep = (rhe_t*)calloc(hsize, sizeof(rhe_t)))) {
-			printf("[Error: calloc failed for hash element]\n");
-			return NULL;
-		}
-		hp->elems_array[i] = ep;
-		
-		ep->queue = qopen();
-		ep->key = NULL;
+	if (!(hp->table = (queue_t**)calloc(hsize, sizeof(queue_t*)))) {
+		printf("[Error: calloc failed for table]\n");
+		return NULL;
 	}
+
+	hp->len = hsize;
+	
+	for(int i = 0; i < hp->len; i++) {
+	  hp->table[i] = qopen();
+	}
+
 	return (hashtable_t*)hp; 	
 }
 
-
 void hclose(hashtable_t *htp) {
 	rht_t *hp;
-	rhe_t *ep;
 
 	hp = (rht_t*)htp;
 
-	for(int i = 0; i < hp->max_len; i++) {
-		ep = hp->elems_array[i];
-		qclose(ep->queue);
-		free(ep);
- 	}
+	for(int i = 0; i < hp->len; i++) {
+	  qclose(hp->table[i]);
+	}
+
+	free(hp->table);
 	free(hp);
 }
 
@@ -87,11 +64,13 @@ int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen) {
 	//  key and keylen (arbitrary str--we can choose) into our SuperFastHash function
 	
 	// cast htp as rht_t
-	rht_hp *hp;
-	int hashvalue; 
+	rht_t *hp;
+	uint32_t hashnumber;
 
-	hp = (rht_hp*)htp;
-	
+	hp = (rht_t*)htp;
+	hashnumber = SuperFastHash(key, keylen, hp->len);
+
+	return qput(hp->table[hashnumber], ep);
 	// call super fast hash and return the value in an int var
 	//   param in superfast hash = param in hput: 
 	//   data = key
@@ -100,35 +79,65 @@ int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen) {
 
 	
 	// index to that returned value in the hash table and store put the ep into the queue there 
+} 
+
+void printe(void *h) {
+	puts((char*)h);
 }
 
+bool keysearch(void* elementp, const void* searchkeyp) {
 
+	char *elp = (char*)elementp;
+	char *kep = (char*)searchkeyp;
+
+	if(strcmp(elp, kep) == 0)
+		return true;
+	else
+		return false;
+
+}
 void happly(hashtable_t *htp, void (*fn)(void* ep)) {
-	//TODO
-}
+	rht_t *hp;
 
+	hp = (rht_t*)htp;
+
+	for(int i = 0; i < hp->len; i++) {
+	  qapply(hp->table[i], fn);
+	}
+}
 
 void *hsearch(hashtable_t *htp,
 							bool (*searchfn)(void* elementp, const void* searchkeyp),
 							const char *key,
 							int32_t keylen) {
-	//TODO
+	rht_t *hp;
+	uint32_t hashnumber;
+
+	hp = (rht_t*)htp;
+	hashnumber = SuperFastHash(key, keylen, hp->len);
+
+	return qsearch(hp->table[hashnumber], searchfn, key);
 }
 
-
+/*
 void *hremove(hashtable_t *htp,
 							bool (*searchfn)(void* elementp, const void* searchkeyp),
 							const char *key,
 							int32_t keylen) {
 	//TODO
 }
-
+*/
 
 int main(void) {
 
-	int32_t size = 3;
+	uint32_t size = 40;
+	char greeting[] = "hello";
 	
 	hashtable_t* hashtable = hopen(size);
+	hput(hashtable, greeting, "hello", 5);
+	happly(hashtable, printe);
+	char* whatever = (char*)hsearch(hashtable, keysearch, "hello", 5);
+	puts(whatever);
 	hclose(hashtable);
 
   exit(EXIT_SUCCESS);
